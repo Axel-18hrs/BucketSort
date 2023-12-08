1635ab649d8e26c66c568d607b2ddc25ee096c8f
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-07 18:05:44 -0600

    .

--- Program.cs
+++ Program.cs
@@ -7,21 +7,22 @@ namespace BucketSort
     internal class Program
     {
         private static Random _rand = new Random();
+
         static void Main(string[] args)
         {
             do
             {
                 Console.Clear();
-                Console.WriteLine("Seleccione una opción:");
-                Console.WriteLine("1. Utilizar el arreglo directamente (números enteros)");
-                Console.WriteLine("2. Utilizar el método que genera un vector (números enteros)");
-                Console.WriteLine("3. Utilizar el arreglo directamente (números decimales)");
-                Console.WriteLine("4. Utilizar el método que genera un vector (números decimales)");
-                Console.WriteLine("0. Salir");
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Use the array directly (integer numbers)");
+                Console.WriteLine("2. Use the method that generates an array (integer numbers)");
+                Console.WriteLine("3. Use the array directly (decimal numbers)");
+                Console.WriteLine("4. Use the method that generates an array (decimal numbers)");
+                Console.WriteLine("0. Exit");
 
                 if (!int.TryParse(Console.ReadLine(), out int option))
                 {
-                    Console.WriteLine("Entrada no válida. Por favor, ingrese un número válido.");
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
                     Console.ReadKey();
                     continue;
                 }
@@ -30,92 +31,92 @@ namespace BucketSort
                 {
                     case 1:
                         int[] array1 = { 4, 2, 3, 5, 5, 7, 1 };
-                        Console.WriteLine("Array antes de ordenar:");
-                        PrintArray(array1);
+                        Console.WriteLine("Array before sorting: [" + string.Join(", ", array1) + "]");
+                        DateTime startTime = DateTime.Now;
                         BucketSort_int(array1);
-                        Console.WriteLine("\nArray después de ordenar:");
-                        PrintArray(array1);
+                        Console.WriteLine("\nArray after sorting: [" + string.Join(", ", array1) + "]");
+                        Console.WriteLine("Time: " + (DateTime.Now - startTime));
                         break;
 
                     case 2:
-                        int[] array2 = GenerarVector();
-                        Console.WriteLine("Array antes de ordenar:");
-                        PrintArray(array2);
+                        int[] array2 = GenerateIntArray();
+                        Console.WriteLine("Array before sorting: [" + string.Join(", ", array2) + "]");
+                        DateTime startTime_ = DateTime.Now;
                         BucketSort_int(array2);
-                        Console.WriteLine("\nArray después de ordenar:");
-                        PrintArray(array2);
+                        Console.WriteLine("\nArray after sorting: [" + string.Join(", ", array2) + "]");
+                        Console.WriteLine("Time: " + (DateTime.Now - startTime_));
                         break;
 
                     case 3:
                         double[] array3 = { 0.42, 0.33, 0.37, 0.57, 0.40 };
-                        Console.WriteLine("Array antes de ordenar:");
-                        PrintArray(array3);
+                        Console.WriteLine("Array before sorting: [" + string.Join(", ", array3) + "]");
+                        DateTime startTim = DateTime.Now;
                         BucketSort_Double(array3);
-                        Console.WriteLine("\nArray después de ordenar:");
-                        PrintArray(array3);
+                        Console.WriteLine("\nArray after sorting: [" + string.Join(", ", array3) + "]");
+                        Console.WriteLine("Time: " + (DateTime.Now - startTim));
                         break;
 
                     case 4:
-                        double[] array4 = GenerarVectorDouble();
-                        Console.WriteLine("Array antes de ordenar:");
-                        PrintArray(array4);
+                        double[] array4 = GenerateDoubleArray();
+                        Console.WriteLine("Array before sorting: [" + string.Join(", ", array4) + "]");
+                        DateTime s_tartTim = DateTime.Now;
                         BucketSort_Double(array4);
-                        Console.WriteLine("\nArray después de ordenar:");
-                        PrintArray(array4);
+                        Console.WriteLine("\nArray after sorting: [" + string.Join(", ", array4) + "]");
+                        Console.WriteLine("Time: " + (DateTime.Now - s_tartTim));
                         break;
 
                     case 0:
                         return;
 
                     default:
-                        Console.WriteLine("Opción no válida. Por favor, elija una opción del 0 al 4.");
+                        Console.WriteLine("Invalid option. Please choose an option from 0 to 4.");
                         break;
                 }
                 Console.ReadKey();
             } while (true);
         }
 
-        public static double[] GenerarVectorDouble(int Minon = 0, int Lenght = 10, int values = 5)
+        public static double[] GenerateDoubleArray(int Min = 0, int Length = 10, int Values = 5)
         {
-            List<double> _List = new List<double>();
+            List<double> list = new List<double>();
 
-            for (int i = Minon; i < Lenght; i++)
+            for (int i = Min; i < Length; i++)
             {
-                if (i < values)
+                if (i < Values)
                 {
-                    double NewValor = _rand.NextDouble();
-                    _List.Add(NewValor);
+                    double newValue = _rand.NextDouble();
+                    list.Add(newValue);
                 }
                 else
                 {
                     break;
                 }
             }
-            return _List.ToArray();
+            return list.ToArray();
         }
 
-        public static int[] GenerarVector(int Minon = 0, int Lenght = 10, int values = 5)
+        public static int[] GenerateIntArray(int Min = 0, int Length = 10, int Values = 5)
         {
-            List<int> _List = new List<int>();
+            List<int> list = new List<int>();
 
-            for (int i = Minon; i < Lenght; i++)
+            for (int i = Min; i < Length; i++)
             {
-                if (i < values)
+                if (i < Values)
                 {
-                    int NewValor = _rand.Next(Minon, Lenght + 1);
-                    if (_List.Contains(NewValor))
+                    int newValue = _rand.Next(Min, Length + 1);
+                    if (list.Contains(newValue))
                     {
                         i--;
                         continue;
                     }
-                    _List.Add(NewValor);
+                    list.Add(newValue);
                 }
                 else
                 {
                     break;
                 }
             }
-            return _List.ToArray();
+            return list.ToArray();
         }
 
         private static void PrintArray(double[] doubles)
@@ -129,32 +130,32 @@ namespace BucketSort
 
         static void BucketSort_Double(double[] array)
         {
-            // Crear buckets vacíos
+            // Create empty buckets
             List<double>[] buckets = new List<double>[array.Length];
             for (int i = 0; i < buckets.Length; i++)
             {
                 buckets[i] = new List<double>();
             }
 
-            // Insertar elementos en sus respectivos buckets
+            // Insert elements into their respective buckets
             foreach (double element in array)
             {
                 buckets[(int)(element * array.Length)].Add(element);
             }
 
-            // Imprimir el estado de los buckets después de la inserción
+            // Print the state of the buckets after insertion
             PrintBucketState(buckets);
 
-            // Ordenar los elementos de cada cubo
+            // Sort the elements in each bucket
             for (int i = 0; i < array.Length; i++)
             {
                 buckets[i].Sort();
             }
 
-            // Imprimir el estado de los buckets después de la ordenación
+            // Print the state of the buckets after sorting
             PrintBucketState(buckets);
 
-            // Obtener los elementos ordenados
+            // Get the sorted elements
             int k = 0;
             for (int i = 0; i < array.Length; i++)
             {
@@ -197,8 +198,7 @@ namespace BucketSort
 
         public static void BucketSort_int(int[] array)
         {
-
-            // Encuentra el valor máximo en el array
+            // Find the maximum value in the array
             int maxVal = array[0];
 
             for (int i = 1; i < array.Length; i++)
@@ -206,28 +206,28 @@ namespace BucketSort
                 if (array[i] > maxVal)
                     maxVal = array[i];
             }
-            // Crea una lista de buckets vacíos
+            // Create a list of empty buckets
             List<int>[] buckets = new List<int>[maxVal + 1];
             for (int i = 0; i < buckets.Length; i++)
             {
                 buckets[i] = new List<int>();
             }
 
-            // Distribuye los elementos en los buckets
+            // Distribute elements into the buckets
             for (int i = 0; i < array.Length; i++)
             {
                 buckets[array[i]].Add(array[i]);
             }
             PrintBucketState(buckets);
 
-            // Ordena cada cubo individualmente
+            // Sort each bucket individually
             for (int i = 0; i < buckets.Length; i++)
             {
                 buckets[i].Sort();
             }
             PrintBucketState(buckets);
 
-            // Concatena los elementos ordenados de cada cubo
+            // Concatenate the sorted elements from each bucket
             int index = 0;
             for (int i = 0; i < buckets.Length; i++)
             {
@@ -237,15 +237,5 @@ namespace BucketSort
                 }
             }
         }
-
-        static void PrintArray(int[] arr)
-        {
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
-
     }
 }
